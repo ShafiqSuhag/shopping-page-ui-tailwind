4a7739eae99120af6fb131b90e034eaa80b35954
Shafiq Suhag <shafiqsuhag@gmail.com>
2021-09-14 20:33:21 +0600

    Good commenting

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
-
+/* -------------------
+    Loading msg 
+---------------------*/
 const loaderContainer = document.getElementById('loader-container');
 const loading = (isLoading) => {
   console.log(isLoading);
@@ -9,8 +11,13 @@ const loading = (isLoading) => {
     loaderContainer.style.visibility = '';
   }
 }
+/* -------------------
+    Loading msg  ./
+---------------------*/
 
-// CALL API 
+/* -------------------
+      CALL API 
+-------------------- */      
 const loadProducts = () => {
   // Clear old products 
   document.getElementById("all-products").innerText = '';
@@ -25,10 +32,15 @@ const loadProducts = () => {
     .then((data) => showProducts(data));
 };
 loadProducts();
-// const jsonData = 'products.json';
-// showProducts(jsonData);
+/* -------------------
+      CALL API ./
+-------------------- */  
 
-// show all product in UI 
+
+
+/* --------------------------
+      show all product in UI  
+---------------------------- */  
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   
@@ -37,39 +49,40 @@ const showProducts = (products) => {
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `
-    <div class="single-product">
-      <div class="product-img-shadow ">
-        <img class="product-image" src=${image}></img>
-        <div class="product-category-badge ">
-          ${product.category}
+      <div class="single-product">
+        <div class="product-img-shadow ">
+          <img class="product-image" src=${image}></img>
+          <div class="product-category-badge ">
+            ${product.category}
+          </div>
         </div>
-      </div>
-      <div class="product-details-container">
-        <div class="product-title d-flex flex-column justify-content-start ">
-          <h4 >${product.title}</h4>
-        </div>
-        <p class="product-rating">     
-          <i class="fas fa-star"></i> ${product.rating.rate} rated by ${product.rating.count} people        
-        </p>
-
-        <h2 class="product-price">Price: $ ${product.price}</h2>
-        
-        
-
-
-        <div class="btn-group mt-3" role="group" >
-          <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn  btn-outline-success btn-sm">ADD TO CART</button>
-          <button id="details-btn" class="btn  btn-outline-info btn-sm ">DETAILS</button>
-        </div>
-      </div>
-      
+        <div class="product-details-container">
+          <div class="product-title d-flex flex-column justify-content-start ">
+            <h4 >${product.title}</h4>
+          </div>
+          <p class="product-rating">     
+            <i class="fas fa-star"></i> ${product.rating.rate} rated by ${product.rating.count} people        
+          </p>
+          <h2 class="product-price">Price: $ ${product.price}</h2>
+          <div class="btn-group mt-3" role="group" >
+            <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn  btn-outline-success btn-sm">ADD TO CART</button>
+            <button id="details-btn" class="btn  btn-outline-info btn-sm ">DETAILS</button>
+          </div>
+        </div>        
       </div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
 };
 
+/* -------------------------------
+      show all product in UI  ./ 
+--------------------------------- */  
 
+
+/* -------------------------------
+      ADD TO CART  
+--------------------------------- */ 
 let count = 0;
 const addToCart = (id, price) => {
   // Update product count 
@@ -80,12 +93,17 @@ const addToCart = (id, price) => {
   updateProductPrice("price", price);
 
   // Calculate Tax & Delivery Charge 
-  updateTaxAndCharge();
-  
+  updateTaxAndCharge();  
+
   // Update Grand Total Price
   updateTotal();
 };
+/* -------------------------------
+      ADD TO CART   ./
+--------------------------------- */
 
+
+// Get Input value from element & convert to float number
 const getInputValue = (elementId) => {
   const element = document.getElementById(elementId).innerText;
   // console.log('updateProductPrice>getInputValue ',element);
@@ -93,39 +111,34 @@ const getInputValue = (elementId) => {
   // const  converted =  parseFloat(element,2) ;                      // convert innerText value to Float
   return converted;
 };
+// Get Input value from element & convert to float number ./
 
 // Update Product Price , Calculate Tax , Calculated total Sum 
 const updateProductPrice = (elementId, value) => {
-  
-
   const oldProductPrice = getInputValue(elementId);
   const currentProductPrice = convertToFloatNumber(value);
   console.log(count, oldProductPrice,currentProductPrice );
   const total = oldProductPrice + currentProductPrice;
   document.getElementById(elementId).innerText = convertToFloatNumber(total) ; 
-  
-  // document.getElementById(id).innerText = Math.round(total);
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
   document.getElementById(id).innerText = value;
-  // document.getElementById(id).innerText = Math.round(value);
 };
+// set innerText function ./
 
+// Convert value to float to number 
 const convertToFloatNumber = (value) => {
-  // return Number.parseFloat(value).toFixed(2);
-  // const toFloat = parseFloat(value);
-  // return Number.tot
-    // return Number(parseFloat(value).toFixed(2));
     return Number(parseFloat(value).toFixed(2));
 }
+// Convert value to float to number  ./
+
 
 // update delivery charge and total Tax
 const updateTaxAndCharge = () => {
   const subTotalProductPrice = getInputValue("price");
   if (subTotalProductPrice > 200) {
-    // console.log (convertToFloatNumber(subTotalProductPrice * 0.2));
     setInnerText("delivery-charge", 30);
     setInnerText("total-tax",   convertToFloatNumber(subTotalProductPrice * 0.2) );
   }
@@ -138,11 +151,13 @@ const updateTaxAndCharge = () => {
     setInnerText("total-tax", convertToFloatNumber(subTotalProductPrice * 0.4) );
   }
 };
+// update delivery charge and total Tax ./
 
-//grandTotal update function
+//Grand Total Price function 
 const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
   document.getElementById("total").innerText = convertToFloatNumber(grandTotal);
 };
+//Grand Total Price function  ./
